d170d6878478dc6b47e0b7d0f54903c7dbfb21fb
AlexDjangoX <alexmonk17@gmail.com>
2022-11-12 12:21:06 +0100

    kanban.js, createKanban controller done

--- controllers/kanban.js
+++ controllers/kanban.js
@@ -3,28 +3,42 @@ import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 
 export const createKanban = async (req, res) => {
-  const kanbanData = req.body;
-  const id = Number(req.params.id.split('|')[1].replace(/[a-z]/gi, ''));
-
   try {
-    if (!kanbanData) {
+    const kanbanData = req.body;
+    const id = Number(req.params.id.split('|')[1].replace(/[a-z]/gi, ''));
+
+    if (!kanbanData || !id) {
       throw new Error('Please provide content');
     }
 
-    const kanban = await prisma.kanban.create({
-      data: { kanbanObject: kanbanData, userId: 2 },
+    const findKanban = await prisma.kanban.findUnique({
+      where: { userId: id },
     });
-    console.log('RESPONSE : ', typeof kanban);
-    return kanban;
-  } catch (error) {
-    console.error(error.message);
+
+    if (!findKanban) {
+      const createdKanban = await prisma.kanban.create({
+        data: { kanbanObject: kanbanData, userId: id },
+      });
+
+      res.status(200).json({ data: createdKanban });
+    } else {
+      const updatedKanban = await prisma.kanban.update({
+        where: { userId: id },
+        data: { kanbanObject: kanbanData, userId: id },
+      });
+
+      res.status(200).json({ data: updatedKanban });
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ status: 'fail', message: err });
   }
 };
 
 export const getKanbanByUserId = async (req, res) => {
   try {
-    const kanban = await prisma.kanban.findMany({});
-    res.json({ kanban });
+    // const kanban = await prisma.kanban.findMany({});
+    // res.json({ kanban });
   } catch (error) {
     console.error(error.message);
   }
