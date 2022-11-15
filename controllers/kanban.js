b063bbda6d66b389a52bf120bd2f4c13bc184f83
AlexDjangoX <alexmonk17@gmail.com>
2022-11-15 15:57:02 +0100

    IN PROGRESS : deleteVerbById() in controllers/kanban.js BUGS : write seed file for DB

--- controllers/kanban.js
+++ controllers/kanban.js
@@ -44,9 +44,54 @@ export const getKanbanByUserId = async (req, res) => {
     if (!foundKanban || !id) {
       throw new Error('Please provide valid user');
     }
-    console.log('FOUND KANBAN : ', foundKanban);
+    // console.log(
+    //   'FOUND KANBAN : ',
+    //   foundKanban.kanbanObject.columnFour.items[1].id
+    // );
     res.status(200).json({ data: foundKanban });
   } catch (error) {
-    console.error(error.message);
+    console.log(err);
+    res.status(404).json({ status: 'fail', message: err });
+  }
+};
+
+export const deleteVerbById = async (req, res) => {
+  try {
+    const verbId = req.body;
+    const kanbanId = Number(req.params.id.split('|')[1].replace(/[a-z]/gi, ''));
+
+    const foundKanban = await prisma.kanban.findUnique({
+      where: { userId: kanbanId },
+    });
+
+    if (!foundKanban || !verbId || !kanbanId) {
+      throw new Error('Please provide valid user 69');
+    }
+    const verbArray = foundKanban.kanbanObject.columnFour.items;
+    const updatedVerbArray = verbArray.filter((el) => el.id !== verbId.verbId);
+
+    const updatedObject = (obj) => {
+      const objClone = JSON.parse(JSON.stringify({ ...obj }));
+      for (let j in objClone) {
+        if (objClone[j].name == 'Stare słowa') {
+          objClone[j].items = updatedVerbArray;
+          console.log(objClone[j].items);
+        }
+      }
+
+      return objClone;
+    };
+
+    const updateKanbanObject = await updatedObject(foundKanban.kanbanObject);
+
+    await prisma.kanban.update({
+      where: { userId: kanbanId },
+      data: { kanbanObject: updateKanbanObject },
+    });
+
+    res.status(200);
+  } catch (err) {
+    console.log(err);
+    res.status(404).json({ status: 'fail', message: err });
   }
 };
--- routes/kanban.js
+++ routes/kanban.js
@@ -1,11 +1,16 @@
 import { Router } from 'express';
-import { getKanbanByUserId } from '../controllers/kanban.js';
-import { createKanban } from '../controllers/kanban.js';
+import {
+  getKanbanByUserId,
+  deleteVerbById,
+  createKanban,
+} from '../controllers/kanban.js';
 
 const router = Router();
 
 router.put('/:id', createKanban);
 
 router.get('/:id', getKanbanByUserId);
+
+router.delete('/:id', deleteVerbById);
 
 export default router;
