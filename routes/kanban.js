d655bc217e3cd888dd88c5549808e6c3001129e2
AlexDjangoX <alexmonk17@gmail.com>
2022-11-16 11:04:10 +0100

    DELETED : route and controller for deleting verb by ID. Done on front-end

--- routes/kanban.js
+++ routes/kanban.js
@@ -1,16 +1,10 @@
 import { Router } from 'express';
-import {
-  getKanbanByUserId,
-  deleteVerbById,
-  createKanban,
-} from '../controllers/kanban.js';
+import { getKanbanByUserId, createKanban } from '../controllers/kanban.js';
 
 const router = Router();
 
 router.put('/:id', createKanban);
 
 router.get('/:id', getKanbanByUserId);
-
-router.delete('/:id', deleteVerbById);
 
 export default router;
