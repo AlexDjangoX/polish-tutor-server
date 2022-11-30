60abeccd1da3a7199564b500d54c04fa30cd1017
AlexDjangoX <alexmonk17@gmail.com>
2022-11-30 15:12:04 +0100

    another build

--- app.js
+++ app.js
@@ -20,15 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
-app.use(express.static(path.join(__dirname + '/public')));
+app.use(express.static(path.join(__dirname + '/build')));
 
 app.get('/', async (req, res, next) => {
   res.send({ message: 'App entry point' });
 });
-
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, '/public/index.html'));
-// });
 
 export const jwtCheck = expressjwt({
   secret: jwks.expressJwtSecret({
