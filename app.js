3d43bea3a02611d5a40c382d0d954371f9b703b5
AlexDjangoX <alexmonk17@gmail.com>
2022-12-06 21:31:32 +0100

    added route to handle manual refresh

--- app.js
+++ app.js
@@ -22,8 +22,12 @@ app.use(morgan('dev'));
 
 app.use(express.static(path.join(__dirname + '/build')));
 
-app.get('/', async (req, res, next) => {
-  res.send({ message: 'App entry point' });
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, '/build/index.html'), function (err) {
+    if (err) {
+      res.status(500).send(err);
+    }
+  });
 });
 
 export const jwtCheck = expressjwt({
